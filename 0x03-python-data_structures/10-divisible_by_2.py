9f80c340a79dd5439b397b772c3d1a09d1e9540e
wambui-mungai <wambuim220@gmail.com>
2023-06-12 13:25:12 -0700

    divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -7,7 +7,7 @@ def divisible_by_2(my_list=[]):
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
             multiples.append(True)
-        elif:
+        else:
             multiples.append(False)
 
     return (multiples)
