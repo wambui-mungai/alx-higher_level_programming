1e419d6aae9186ff9c69e5591dd28ead43b131f5
wambui-mungai <wambuim220@gmail.com>
2023-06-14 14:02:50 -0700

    besr score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    best_key = None
-    best_value = float('-inf')
+    if not a_dictionary:
+        return (None)
 
-    for key, value in a_dictionary.items():
-        if value > best_value:
-            best_key = key
-            best_value = value
-
-    return best_key
+    return (max(a_dictionary, key=a_dictionary.get))
