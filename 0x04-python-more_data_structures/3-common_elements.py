6e1f1fe1a6f83988ea8428782cbc6f7c16097ab0
wambui-mungai <wambuim220@gmail.com>
2023-06-14 13:18:36 -0700

    common elemeents

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common = set()
-    for element in set_1:
-        if element in set_2:
-            common.add(element)
-    return common
-
+    return (set_1 & set_2)
