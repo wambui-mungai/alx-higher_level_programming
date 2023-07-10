632ad2f05b0f86146cc0a3b1f53315b8aaf59b23
wambui-mungai <wambuim220@gmail.com>
2023-07-10 07:34:54 -0700

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""the module defines an object attribute lookup function."""
+
+
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""it defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
